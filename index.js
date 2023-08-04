e29b1b11fb8e7c9785eb708288458f4c8a2eeadd
Sudeep Sharma <sharma.sudip1122@gmail.com>
2023-08-04 19:06:33 +0545

    Passed tests and refactor.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-function isContained(range1, range2, containmentType = 'fully') {
-  if (containmentType === 'fully') {
-    return range1[0] >= range2[0] && range1[1] <= range2[1];
-  } else if (containmentType === 'partially') {
+function isContained(range1, range2, containmentType = 'full') {
+  if (containmentType === 'full') {
+    return (
+      range1[0] >= range2[0] && range1[1] <= range2[1] ||
+      range1[0] <= range2[0] && range1[1] >= range2[1]
+    );
+  } else if (containmentType === 'part') {
     return (
       (range1[0] >= range2[0] && range1[0] <= range2[1]) ||
       (range1[1] >= range2[0] && range1[1] <= range2[1]) ||
@@ -9,7 +12,7 @@ function isContained(range1, range2, containmentType = 'fully') {
       (range2[1] >= range1[0] && range2[1] <= range1[1])
     );
   } else {
-    throw new Error('Invalid containment type. Use "fully" or "partially".');
+    throw new Error('Invalid containment type. Use "full" or "part".');
   }
 }
 
